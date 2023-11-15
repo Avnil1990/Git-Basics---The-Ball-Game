76f000b1fd1319d8a648d61d9144e54b9e4d831f
Avnil <avniel.dav@gmail.com>
2023-11-15 18:47:05 +0200

    Change onBallClick() to increment the ball’s width & height by 50px and display the updated diameter inside the ball

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 'use strict'
 
+var ball1Size = 100
+
 function onBallClick() {
+    ball1Size += 50
     console.log('hi')
+    const ball1 = document.querySelector('.ball1')
+    ball1.style.width = ball1Size + 'px'
+    ball1.style.height = ball1Size + 'px'
+    ball1.innerText = ball1Size
+
 }
